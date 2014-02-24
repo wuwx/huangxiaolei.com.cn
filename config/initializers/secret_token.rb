6d21280432cf2fd2dd5b1c9bd8da2ff7441d4989
smallgoldstone <smallgoldstone@qq.com>
2014-02-24 21:11:04 +0800

    初始化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HuangxiaoleiComCn::Application.config.secret_key_base = '3339dbd4a8b84c09a530e2e8aa50a5b7f6c7f90dff8b5d89884a0b1db3a1d8e6e8df718ef7c03cc2a159be6891c0b036a6c3720261e326e40e94085da3da33b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HuangxiaoleiComCn::Application.config.session_store :cookie_store, key: '_huangxiaolei_com_cn_session'
